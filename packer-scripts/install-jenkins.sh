e8ab0620f4ad8f7f8cae2f7bef1685ac91338821
Jay Gala <gala.jay1@northeastern.edu>
2025-01-31 13:23:25 -0500

    Packer scripts for installing Jenkins, Nginx and Certbot

---
+++ packer-scripts/install-jenkins.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+sudo apt update -y
+sudo apt upgrade -y
+sudo apt install openjdk-17-jdk -y
+curl -fsSL https://pkg.jenkins.io/debian/jenkins.io-2023.key | sudo tee /usr/share/keyrings/jenkins-keyring.asc > /dev/null
+echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] https://pkg.jenkins.io/debian binary/ | sudo tee /etc/apt/sources.list.d/jenkins.list > /dev/null
+sudo apt update -y
+sudo apt install jenkins -y
+sudo systemctl enable jenkins
