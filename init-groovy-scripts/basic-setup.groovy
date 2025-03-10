192d6f6bfeb17c932eeabdda7b823a5bb839cb34
Jay Gala <gala.jay1@northeastern.edu>
2025-03-10 06:34:51 -0400

    Removed comments

--- init-groovy-scripts/basic-setup.groovy
+++ init-groovy-scripts/basic-setup.groovy
@@ -2,10 +2,8 @@ import jenkins.model.*
 import hudson.security.*
 import java.util.logging.Logger
 
-// Logger for debugging
 def logger = Logger.getLogger("JenkinsSetupScript")
 
-// Load environment variables from /etc/jenkins.env
 def props = new Properties()
 def envFile = new File('/etc/jenkins.env')
 if (envFile.exists()) {
@@ -17,7 +15,6 @@ if (envFile.exists()) {
 def user = props.getProperty('JENKINS_ADMIN_USER')
 def password = props.getProperty('JENKINS_ADMIN_PASSWORD')
 
-// Validate username and password
 if (!user || !password) {
     throw new RuntimeException("JENKINS_USER or JENKINS_PASSWORD is missing in /etc/jenkins.env")
 }
@@ -27,20 +24,11 @@ if (instance == null) {
     throw new RuntimeException("Jenkins instance is null!")
 }
 
-// Configure security realm
 def hudsonRealm = new HudsonPrivateSecurityRealm(false)
-
-// Check if user already exists to prevent recreation
-
 hudsonRealm.createAccount(user, password)
 logger.info("Admin user '${user}' created '${password}' successfully.")
-
-    
-
-
 instance.setSecurityRealm(hudsonRealm)
 
-// Set authorization strategy
 def strategy = new FullControlOnceLoggedInAuthorizationStrategy()
 strategy.setAllowAnonymousRead(false)
 instance.setAuthorizationStrategy(strategy)
