bea2152844cd5af26fd5a3b95eccf7c13da8f923
Jay Gala <gala.jay1@northeastern.edu>
2025-03-19 16:52:48 -0400

    feat: jenkins pipeline to check for conventional commits

---
+++ multibranch-pipeline-jobs/api-server/docker-image-creation.groovy
@@ -0,0 +1,32 @@
+folder('api-server')
+multibranchPipelineJob('api-server/docker-image-creation') {
+    branchSources {
+        github {
+            id('cyse7125-sp25-team02')
+            repoOwner('cyse7125-sp25-team02')
+            repository('api-server')
+            checkoutCredentialsId('github-credentials')
+            scanCredentialsId('github-credentials')
+        }
+    }
+
+    configure { node ->
+        def traits = node / sources / data / 'jenkins.branch.BranchSource' / source / traits
+        
+        traits << 'org.jenkinsci.plugins.github_branch_source.BranchDiscoveryTrait' {
+            strategyId(1)
+        }
+        
+        traits << 'org.jenkinsci.plugins.githubScmTraitNotificationContext.NotificationContextTrait' {
+            contextLabel('docker-image-creation')
+            typeSuffix(false)
+            multipleStatuses(false)
+        }
+    }
+
+    factory {
+        workflowBranchProjectFactory {
+            scriptPath('jenkinsfiles/Jenkinsfile.docker-image-creation')
+        }
+    }
+}
