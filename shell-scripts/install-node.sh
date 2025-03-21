1e2e3f0fca59ef3ce4e1fe2e2ad7526104c37c7f
Jay Gala <gala.jay1@northeastern.edu>
2025-03-21 11:13:48 -0400

    feat: jenkins DSL for database-migration and helm-charts

--- shell-scripts/install-node.sh
+++ shell-scripts/install-node.sh
@@ -2,5 +2,6 @@
 set -x
 
 sudo apt update -y
+curl -fsSL https://deb.nodesource.com/setup_current.x | sudo -E bash -
 sudo apt install nodejs npm -y
 sudo npm install -g @commitlint/cli @commitlint/config-conventional
